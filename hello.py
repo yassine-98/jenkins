0c7f480c1c09a559ce24e189151dc0e2505294c3
Yassine EL BAHI <yassineelbahi98@gmail.com>
2022-10-10 15:16:48 +0200

    first commit

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print('salut Yassine')
+import socket
+
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def index():
+    
+    var=socket.gethostname()
+
+    return render_template('index.html.j2',var=var)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8090,debug=True)
+
